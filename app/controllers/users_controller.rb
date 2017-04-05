ede14623139c70730c09f0cb4e5409892c0d9966
mattcheang <cheangmatt@gmail.com>
2017-04-05 16:59:23 -0700

    setup home page and user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  before_action :require_sign_in
+
+  def show
+   @user = User.find(params[:id])
+ end
+
+ def edit
+   @user = User.find(params[:id])
+ end
+
+ def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Profile was updated successfully."
+      redirect_to @user
+    else
+      flash.now[:alert] = "There was an error updating your profile. Please try again."
+    end
+  end
+
+ private
+
+ def user_params
+   params.require(:user).permit(:username, :description, :image)
+ end
+end
 end
