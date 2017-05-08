92058261cd367b8b87fed90d63d14c4ae6dcde44
mattcheang <cheangmatt@gmail.com>
2017-05-08 16:16:46 -0700

    edit user and messages styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,10 @@ module UsersHelper
     current_user == @user
   end
 
+  def user_followers_nil?
+    @user.followers.count == 0
+  end
+
   def current_user_is_following(current_user_id, followed_user_id)
     relationship = Follow.find_by(follower_id: current_user_id, following_id: followed_user_id)
     return true if relationship
